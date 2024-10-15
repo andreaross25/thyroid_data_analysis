0600e7f94609520d405eec18d74e1cf04e21457f
Andrea Ross <andreaross.ibt@gmail.com>
2024-10-15 13:33:08 -0700

    Frecuencia de los datos por grupo y por categoría Bethesda

--- data_analysis.py
+++ data_analysis.py
@@ -16,4 +16,39 @@ import seaborn as sns
 
 # Cargar datos
 data = pd.read_excel('thyroid_merged.xlsx')
-print(data.head())
+
+# Obtener análisis descriptivo de variables numéricas para todos los pacientes
+description = data.describe()
+
+# Obtener análisis descriptivo de variables numéricas por grupo
+grouped_description = data.groupby('grupo_x').describe()
+
+# Crear gráfico de barras para la frecuencia de cada grupo y categoría Bethesda
+palette = 'Paired'
+ax1 = sns.countplot(x='grupo_x', data=data, palette=palette)
+ax1.set_title("Pacientes por grupo")
+ax1.set_xlabel("Grupo")
+ax1.set_ylabel("Frecuencia")
+
+# Agregar etiquetas de frecuencia en las barras
+for p in ax1.patches:
+    ax1.annotate(f'{int(p.get_height())}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                 ha='center', va='baseline', fontsize=8, color='black', xytext=(0, 3),
+                 textcoords='offset points')
+
+plt.show()
+
+# Crear el gráfico de barras para 'bethesda_x'
+ax2 = sns.countplot(x='bethesda_x', data=data, palette=palette)
+ax2.set_title("Pacientes por categoría Bethesda")
+ax2.set_xlabel("Categoría Bethesda")
+ax2.set_ylabel("Frecuencia")
+
+# Agregar etiquetas de frecuencia en las barras
+for p in ax2.patches:
+    ax2.annotate(f'{int(p.get_height())}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                 ha='center', va='baseline', fontsize=8, color='black', xytext=(0, 3),
+                 textcoords='offset points')
+
+plt.show()
+
